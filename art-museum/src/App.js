a3d86bbf3ae4a8a8dfe95c5314c534eb67458b14
farivar <farivaramiri@gmail.com>
2023-07-28 17:01:59 -0700

    done

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,7 +1,12 @@
+import { harvardArt } from './data/harvardArt';
+
+
 function App() {
   return (
     <h1>Hello from App</h1>
   );
 }
+
+console.log(harvardArt);
 
 export default App;
--- art-museum/src/index.js
+++ art-museum/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Root />,
   document.getElementById('root')
 );
+
+
+function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
